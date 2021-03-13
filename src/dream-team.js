60eb338c74a624964164ccf6578a856504f7b5a4
Kirill Beliakov <kabeliakov@gmail.com>
2021-03-13 10:29:29 +0300

    feat: add solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!members || members.length === 0) return false;
+  let ans = [];
+  let str;
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == 'string') {
+      str = members[i].trimStart().split(' ');
+      ans.push(str[0][0]);
+    }
+  }
+  return ans.sort().join('').toUpperCase()
+}
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let ans = [],
+      discard_next = '--discard-next',
+      discard_prev = '--discard-prev',
+      double_next = '--double-next',
+      double_prev = '--double-prev'
+  for (let i = 0; i < arr.length; i ++) {
+    if (arr[i] === discard_next) {
+      
+    } else if (arr[i] === discard_prev) {
+
+    } else if (arr[i] === double_next) {
+
+    } else if (arr[i] === double_prev) {
+
+    }
+  }
 };
